8878cca75839801490223e05b7ef0b87eda9b08d
Muhammad Hamza Ali <hamzahuzaifa104@gmail.com>
2024-11-15 12:37:36 +0500

    main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { CounterProdiver  } from './Context/Counter.jsx'
+import Cart from './Context/Cart.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Cart>
+    <CounterProdiver>
     <App />
+    </CounterProdiver>
+    </Cart>
   </StrictMode>,
 )
